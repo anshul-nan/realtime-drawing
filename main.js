29bb160078b6817ffd6095905e8ca26948ca4e64
anshul-nan <105433706+anshul-nan@users.noreply.github.com>
2023-03-16 17:40:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function modelLoaded()
 
 function gotPoses(results)
 {
-    if(results.lenght>0)
+    if(results.length>0)
     {
         console.log(results);
         noseX =results[0].pose.nose.x;
